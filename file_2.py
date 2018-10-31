5ac5122e5974e96248f1bbda93287a977a1f8c47
Ashank <ashank135b@gmail.com>
2018-10-31 09:45:18 +0530

    Second Commit

--- file_2.py
+++ file_2.py
@@ -13,7 +13,6 @@ from nltk.stem.snowball import SnowballStemmer
 # Multi-frame tkinter application v2.2
 import tkinter as tk
 
-
 input = ""
 final_output = {}
 
@@ -28,6 +27,7 @@ with open("storedata.txt", "rb") as myFile:
 with open("storewords.txt", "rb") as myFile:
     all_words = pickle.load(myFile)
 
+
 def percentageCalculator(x, y, case=1):
     """Calculate percentages
        Case1: What is x% of y?
@@ -36,7 +36,7 @@ def percentageCalculator(x, y, case=1):
     """
     if case == 1:
         # Case1: What is x% of y?
-        r = x / 100 * y
+        r = x / 100.0 * y
         return r
     elif case == 2:
         # Case2: x is what percent of y?
@@ -48,7 +48,6 @@ def percentageCalculator(x, y, case=1):
         return r
     else:
         raise Exception("Only case 1,2 and 3 are available!")
-
 
 
 def query_vector(query):
@@ -77,21 +76,21 @@ def query_rank(document, query):
     :type query: dict
     :type document: dict
     """
-    sumxx = 0
-    sumyy = 0
-    sumxy = 0
-    for term in idf.keys():
-        sumxx += document[term] * document[term]
-        sumyy += query[term] * query[term]
-        sumxy += query[term] * document[term]
+
+    sumxx = sum(document[k] * document[k] for k in idf.keys())
+    sumyy = sum(query[k] * query[k] for k in idf.keys())
+    sumxy = sum(document[k] * query[k] for k in idf.keys())
+    # for term in idf.keys():
+    #   sumxx += document[term] * document[term]
+    #   sumyy += query[term] * query[term]
+    #   sumxy += query[term] * document[term]
     try:
         return float(sumxy) / math.sqrt(sumxx * sumyy)
     except Exception:
         return 0.0
 
-query_rank  = np.vectorize(query_rank,cache=True)
 
-def return_list(master,query):
+def return_list(master, query):
     """
 
     :param master: master
@@ -105,7 +104,6 @@ def return_list(master,query):
     i = 1
 
     for story in data.keys():
-
         unit = percentageCalculator(i, 1000, case=2)
         step = "Working on {}...".format(i)
         # log.write(str('\n[OK]'))
@@ -117,8 +115,10 @@ def return_list(master,query):
         outputdata[story] = {"index": data[story]['index'], "genre": data[story]['genre']}
         print(i)
         i += 1
-
         master.update()
+
+    # output = dict((k,query_rank(data[k]['tf'],query=query_vec)) for k in data.keys())
+    # outputdata = dict((k,{"index":data[k]['index'],'genre':data[k]['genre']}) for k in data.keys())
     return sorted(output.items(), key=itemgetter(1), reverse=True), outputdata
 
 
@@ -138,6 +138,7 @@ class SampleApp(tk.Tk):
 
 
 Selected_novel = ""
+
 
 class AutocompleteEntry(Entry):
     def __init__(self, lista, *args, **kwargs):
@@ -218,40 +219,36 @@ class AutocompleteEntry(Entry):
 
 
 class StartPage(tk.Frame):
-
     def __init__(self, master):
         tk.Frame.__init__(self, master)
         self.master.title("App")
-        #self.master.geometry("{0}x{1}+0+0".format(
-         #   self.master.winfo_screenwidth(), self.master.winfo_screenheight()))
+        # self.master.geometry("{0}x{1}+0+0".format(
+        #   self.master.winfo_screenwidth(), self.master.winfo_screenheight()))
 
-        #self.cb = tk.Label(self, font=("Arial", 15), text="button changes here...")
+        # self.cb = tk.Label(self, font=("Arial", 15), text="button changes here...")
         global all_words
-        self.start_label = tk.Label(self , font=("Arial" , 15) , text="Enter Text to search:")
+        self.start_label = tk.Label(self, font=("Arial", 15), text="Enter Text to search:")
         self.entry = AutocompleteEntry(all_words, self)
 
         """bottomframe=tk.Frame
         bottomframe.pack(self, side= "bottom")"""
-        self.page_1_search = tk.Button(self , text="Search",
-                                  command=lambda: self.submit(self.entry.get(), master))
-        #self.page_1_advsearch = tk.Button(self, text="Advanced Search",
-                                  #command=lambda: self.advsubmit(self.entry.get(), master))
+        self.page_1_search = tk.Button(self, text="Search",
+                                       command=lambda: self.submit(self.entry.get(), master))
+        # self.page_1_advsearch = tk.Button(self, text="Advanced Search",
+        # command=lambda: self.advsubmit(self.entry.get(), master))
 
-        self.start_label.pack(padx=(10,200) , pady= (100,0), side="top", fill="x")
-        self.entry.pack(padx=(10,30), pady=(10,20) , ipady=3 , side="top" , fill="x")
-        self.page_1_search.pack(padx=(0,30) ,pady=(0,100), anchor='se')
-        #self.page_1_advsearch.pack(side="left")
-        #self.cb.pack()
-        self.percent = tk.Label(self, text="0%", width=4 ,relief='sunken', anchor='se')
-        self.progress = Progressbar(self, length=250, mode='determinate' )
+        self.start_label.pack(padx=(10, 200), pady=(100, 0), side="top", fill="x")
+        self.entry.pack(padx=(10, 30), pady=(10, 20), ipady=3, side="top", fill="x")
+        self.page_1_search.pack(padx=(0, 30), pady=(0, 100), anchor='se')
+        # self.page_1_advsearch.pack(side="left")
+        # self.cb.pack()
+        self.percent = tk.Label(self, text="0%", width=4, relief='sunken', anchor='se')
+        self.progress = Progressbar(self, length=250, mode='determinate')
         self.status = tk.Label(self, text="progress tracker", relief='sunken', anchor='sw')
-
-
 
         self.percent.pack(side='left')
         self.progress.pack(side='left')
         self.status.pack(side='left')
-
 
     def submit(self, e, master):
 
@@ -262,12 +259,13 @@ class StartPage(tk.Frame):
             input += e
 
             global final_output
-            final_output = return_list(self,input)
+            final_output = return_list(self, input)
 
             output = final_output[0]
 
             if output[0][1] == 0:
                 tk.messagebox.showinfo('Oops!!', "nothing to display !!")
+                self.page_1_search['state'] = 'normal'
                 input = ""
             else:
                 master.switch_frame(PageOne)
@@ -286,10 +284,7 @@ class StartPage(tk.Frame):
             i.pack()'''
 
 
-
-
 class PageOne(tk.Frame):
-
     def __init__(self, master):
         tk.Frame.__init__(self, master)
         self.master.title("App")
@@ -302,8 +297,7 @@ class PageOne(tk.Frame):
         page_1_label = tk.Label(self, text="Output:")
         page_1_label.pack(side="top", fill="x", pady=10)
 
-
-        if output[0][1] !=0:
+        if output[0][1] != 0:
             b1 = tk.Button(self, text=output[0][0], command=lambda: self.switch(master, output[0][0]))
             b1.pack(side="top", fill="x", padx=(70, 70), pady=10)
         if output[1][1] != 0:
@@ -323,7 +317,6 @@ class PageOne(tk.Frame):
                                  command=lambda: self.BACK(master))
         start_button.pack(side="bottom", pady=50)
 
-
     def BACK(self, master):
         global final_output
         final_output = {}
@@ -342,23 +335,24 @@ class Selected_Page(tk.Frame):
     def __init__(self, master):
         tk.Frame.__init__(self, master)
         self.master.title("App")
-        #self.master.geometry('1000x1000')
+        # self.master.geometry('1000x1000')
         '''self.master.geometry("{0}x{1}+0+0".format(
             self.master.winfo_screenwidth(), self.master.winfo_screenheight()))'''
-        #self.master.bind('<Escape>', self.toggle_geom)
+        # self.master.bind('<Escape>', self.toggle_geom)
         global final_output
         global text
         global Selected_novel
 
-        txt = "\t\t\t\t" + Selected_novel + "\n" + text[6 * final_output[1][Selected_novel]['index']+6]
-        #self.ans = tk.Label(self, relief='sunken', height=50, text=' ', wraplength=500)
+        txt = "\t\t\t\t" + Selected_novel + "\n" + text[6 * final_output[1][Selected_novel]['index'] + 6]
+        # self.ans = tk.Label(self, relief='sunken', height=50, text=' ', wraplength=500)
 
 
 
-        self.frm=tk.Frame(self)
+        self.frm = tk.Frame(self)
         self.scrollbarV = tk.Scrollbar(self.frm, orient='vertical')
         self.scrollbarH = tk.Scrollbar(self.frm, orient='horizontal')
-        self.txtItem = tk.Text(self.frm, wrap='word', yscrollcommand=self.scrollbarV.set, xscrollcommand=self.scrollbarH.set,
+        self.txtItem = tk.Text(self.frm, wrap='word', yscrollcommand=self.scrollbarV.set,
+                               xscrollcommand=self.scrollbarH.set,
                                relief='sunken', takefocus=0, borderwidth=1, state='normal', cursor='arrow')
         self.txtItem.grid(row=5, column=0, columnspan=7, sticky='n' + 'w' + 's' + 'e')
 
@@ -369,47 +363,51 @@ class Selected_Page(tk.Frame):
 
         self.txtItem.config(state='normal')
         self.txtItem.delete(1.0, 'end')
-        self.txtItem.insert('insert',txt)
+        self.txtItem.insert('insert', txt)
         # self.txtItem.insert(INSERT, rss_item.link, 'a')
         self.txtItem.config(state='disabled')
 
         global input
-        inputstr=input.split(' ')
+        inputstr = input.split(' ')
         with open("storetrie.txt", "rb") as myFile:
             trie = pickle.load(myFile)
 
         start = 1.0
 
+        inputstr = [inp.capitalize() for inp in inputstr]
+        print(inputstr)
+        all_words = []
         for inp in inputstr:
-            all_words = list(trie.all_words_beginning_with_prefix(inp))
+            all_words += list(trie.all_words_beginning_with_prefix(inp))
+        all_words = sorted(all_words, reverse=True)
 
+        print(all_words)
         for word in all_words:
             print(word)
-            pos = self.txtItem.search(' '+word, start, stopindex='end')
+            pos = self.txtItem.search(word, start, stopindex='end')
             while pos:
-                length = len(' '+word)
+                length = len(word)
                 row, col = pos.split('.')
                 end = int(col) + length
                 end = row + '.' + str(end)
                 self.txtItem.tag_add('highlight', pos, end)
                 start = end
-                pos = self.txtItem.search(' ' + word, start, stopindex='end')
+                pos = self.txtItem.search(word, start, stopindex='end')
             self.txtItem.tag_config('highlight', background='white', foreground='red')
 
-
-        #self.ans.pack(side='top',pady=50)
+        # self.ans.pack(side='top',pady=50)
         self.frm.pack(side='top')
         start_button = tk.Button(self, text="Search another word!!!",
                                  command=lambda: self.switch(master))
-        start_button.pack(side='right',padx=20 ,pady=20)
+        start_button.pack(side='right', padx=20, pady=20)
 
         back_button = tk.Button(self, text="Back",
-                                 command=lambda: self.back(master))
-        back_button.pack(side="right",padx=10,pady=20)
+                                command=lambda: self.back(master))
+        back_button.pack(side="right", padx=10, pady=20)
 
     def switch(self, master):
         global input
-        input=""
+        input = ""
         global Selected_novel
         self.txtItem.delete(1.0, 'end')
         Selected_novel = ""
@@ -425,77 +423,3 @@ class Selected_Page(tk.Frame):
 if __name__ == "__main__":
     app = SampleApp()
     app.mainloop()
-
-
-'''
-class PageOne(tk.Frame):
-    def __init__(self, master):
-        ''' '''global input
-        global final_output
-        final_output = return_list(input)''''''
-        output = final_output[0]
-        print(output[0][1])
-
-        tk.Frame.__init__(self, master)
-        self.master.title("App")
-        #self.master.geometry("{0}x{1}+0+0".format(
-            #self.master.winfo_screenwidth(), self.master.winfo_screenheight()))
-
-        button_display = tk.Frame(self)
-        page_1_label = tk.Label(button_display, text="Output:")
-        page_1_label.pack(side="top", fill="x", pady=10)
-        i = 0
-        for out in output:
-            if i == 5 or out[1] == 0:
-                break
-            print(out[0])
-            b = tk.Button(button_display, text=out[0], command=lambda: self.switch(master, out))
-            b.pack(side="top", fill="x", padx=(100,100) ,pady=10)
-            i += 1
-        print(final_output)
-        input = ""
-        start_button = tk.Button(button_display, text="Back",
-                                 command=lambda: self.BACK(master))
-        start_button.pack(side="bottom")
-
-        button_display.pack(side='left')
-
-        #scrollv1=tk.Scrollbar(self,orient='vertical')
-        #scrollh1 = tk.Scrollbar(self, orient='horizontal')
-        scrollv2 = tk.Scrollbar(self, orient='vertical')
-        scrollh2 = tk.Scrollbar(self, orient='horizontal')
-        self.ans = tk.Label(self, relief='sunken', text=" the answer will come here" ,height=200 ,width=150)
-
-        #scrollv2.grid(row=1,column=4,sticky='n'+'s')
-        #scrollv2.config(command=self.ans.yview)
-
-        #scrollh2.grid(row=2, column=0,columnspan=4, sticky='e'+'w')
-        #scrollh2.config(command=self.ans.xview)
-        self.ans.pack(side='left')
-
-
-    def BACK(self, master):
-        global final_output
-        final_output = {}
-        return master.switch_frame(StartPage)
-
-    def switch(self, master, out):
-        global Selected_novel
-        Selected_novel = out
-        #return master.switch_frame(Selected_Page)
-
-        #self.master.geometry('1000x1000')
-        #self.master.geometry("{0}x{1}+0+0".format(
-            #self.master.winfo_screenwidth(), self.master.winfo_screenheight()))
-        #self.master.bind('<Escape>', self.toggle_geom)
-        global final_output
-        print(final_output)
-        global text
-
-        str= text[6 * final_output[1][Selected_novel[0]]['index'] + 6]
-
-        self.ans['text']=str
-        #start_button = tk.Button(self, text="Search another word!!!",
-                                 #command=lambda: master.switch_frame(StartPage))
-        #start_button.pack(side="bottom")
-'''
